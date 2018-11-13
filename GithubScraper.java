9a89c1d693c4c66f60529411009e89d1bafd6094
patrickbeekman1095 <patrickbeekman1095@gmail.com>
2018-11-12 20:26:28 -0500

    able to curl a request to the github api.

--- GithubScraper.java
+++ GithubScraper.java
@@ -1,3 +1,8 @@
+import java.net.URL;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.lang.StringBuilder;
+import java.net.URLConnection;
 
 public class GithubScraper {
     
@@ -5,8 +10,32 @@ public class GithubScraper {
         
     }
 
-    public static void main(String[] args) {
+    public void printCURL(String url) throws IOException {
+
+        URL urlObj = new URL(url);
+        URLConnection uc = urlObj.openConnection();
+
+        uc.setRequestProperty("X-Requested-With", "Curl");
+
+        InputStreamReader inputStream = new InputStreamReader(uc.getInputStream());
+        // Read the input stream
+        final int bufferSize = 1024;
+        final char[] buffer = new char[bufferSize];
+        final StringBuilder out = new StringBuilder();
+        //Reader in = new InputStreamReader(inputStream, "UTF-8");
+        for (; ; ) {
+            int rsz = inputStream.read(buffer, 0, buffer.length);
+            if (rsz < 0)
+                break;
+            out.append(buffer, 0, rsz);
+        }
+        System.out.print(out.toString());
+    }
+
+    public static void main(String[] args) throws IOException {
         System.out.println("hi");
+        GithubScraper scraper = new GithubScraper();
+        scraper.printCURL("https://api.github.com/repos/CompassSoftware/GDET-Four-Musketeers/issues");
     }
 
 }
